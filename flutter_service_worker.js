82974bc432b9c884244f6b309b72e5461cb36d58
Divyang <divyang.techavidus@gmail.com>
2021-03-31 18:46:34 +0530

    build without CC

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "7ae35b9aa37ae7834545ec6867fc20fc",
-"/": "7ae35b9aa37ae7834545ec6867fc20fc",
+  "index.html": "e723359440fb2df8a0d5555e8f347f14",
+"/": "e723359440fb2df8a0d5555e8f347f14",
 "manifest.json": "a83bbbd9d23ea4d752fbcd44cdc544fa",
 "version.json": "426313f2f3133c2f20415344c4a22df3",
 "assets/NOTICES": "6b4e777f9043799d4e31d2f634a7d5d0",
@@ -19,7 +19,7 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "00bb2b684be61e89d1bc7d75dee30b58",
 "icons/Icon-512.png": "cd31a19f835fa24f3cdb8e811e0294ee",
 "icons/Icon-192.png": "a24a6970749b97e3339912b5de71fcdb",
-"main.dart.js": "235a29eebe0a39c6bc6382b98cdfadaf",
+"main.dart.js": "43f3c7b04725f9d9a31b296f610113ba",
 "favicon.png": "83642a3ed4edb7d1f9d58a6244efbe28"
 };
 
